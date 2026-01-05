eb01ad82fe7aa66ebf69417a4a8ff620ee7b25e8
Shahed Nazzal <shahednazzall.777@gmail.com>
2026-01-05 12:46:15 +0000

    save changes

--- web_backend/halal_haram/app.py
+++ web_backend/halal_haram/app.py
@@ -3,10 +3,24 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
+import requests
 
 app = Flask(__name__)
 
-model = load_model("HALAL_HARAM_model.keras")
+# رابط موديل Dropbox
+MODEL_URL = "https://www.dropbox.com/scl/fi/xwh1k4kfnre1s8xlqqgpe/HALAL_HARAM_model.keras?rlkey=sd3fc82dygizzt6w0g7ewxlc8&dl=1"
+MODEL_PATH = "HALAL_HARAM_model.keras"
+
+# تحميل الموديل إذا مش موجود محلياً
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Dropbox...")
+    r = requests.get(MODEL_URL)
+    with open(MODEL_PATH, "wb") as f:
+        f.write(r.content)
+    print("Model downloaded!")
+
+# تحميل الموديل
+model = load_model(MODEL_PATH)
 IMG_SIZE = (224, 224)
 
 @app.route("/predict", methods=["POST"])
@@ -23,12 +37,10 @@ def predict():
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img)[0][0]
-
     result = "Haram" if pred > 0.5 else "Halal"
 
-    return jsonify({ "result": result })
+    return jsonify({"result": result})
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
