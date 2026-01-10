b744b739bfcaaf9ab973563252c201cc804d9318
Shahed Nazzal <shahednazzall.777@gmail.com>
2026-01-10 16:57:23 +0000

    save changes

--- .next/dev/server/chunks/ssr/_73f67bf4._.js
+++ .next/dev/server/chunks/ssr/_73f67bf4._.js
@@ -940,7 +940,7 @@ function HomePage() {
                         children: [
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["jsxDEV"])("h1", {
                                 className: "text-4xl font-bold tracking-tight text-balance sm:text-5xl md:text-6xl lg:text-7xl",
-                                children: "Everything You Need for Wellness"
+                                children: "Everythinggg You Need for Wellness"
                             }, void 0, false, {
                                 fileName: "[project]/app/page.tsx",
                                 lineNumber: 54,
